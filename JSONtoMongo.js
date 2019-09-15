d2e92f6786b6cfbb071b830922d7e6b448fbb83e
Sneha P <snehashrey17@gmail.com>
2019-09-15 09:12:12 -0400

    finished

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -9,9 +9,12 @@ var fs = require('fs'),
     Listing = require('./ListingSchema.js'), 
     config = require('./config');
 
+var listingData;    
+
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, { useNewUrlParser: true });
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,6 +23,39 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+fs.readFile('listings.json', 'utf8', function(err, data)
+{
+  if(err)
+  {
+    throw err;
+  }
+
+ 
+listingData = JSON.parse(data);
+
+//The forEach() method executes a provided function once for each array (listingData.entries) element.  
+listingData.entries.forEach(function(element) 
+{
+  //Listing is a mongoose model
+  var newListing = Listing(
+    {
+    code: element.code,
+    name: element.name,
+    coordinates: element.coordinates,
+    address: element.address
+    });
+
+  newListing.save(function(err) 
+  {
+    if (err) throw err;
+  
+    console.log('User created!');
+  });
+
+});
+
+});
+
 
 
 /*  
